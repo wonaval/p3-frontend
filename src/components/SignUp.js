5bf6558a3489a814c05debc85e729ea7efcac50f
wonaval <wonaval@gmail.com>
2021-12-18 23:35:59 -0500

    Added SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,30 +1,31 @@
-import { useState } from 'react'
-
-import axios from 'axios'
+// Module imports
+import { useState, useContext } from 'react'
 import env from 'react-dotenv'
+import axios from 'axios'
 
-const SignUp = () => {
+// useContext import
+import { UserContext } from '../context/UserContext'
+
+const Signup = () => {
+  // useContext
+  const { userState } = useContext(UserContext)
+  const [ user, setUser ] = userState
+
   // useState
   const [ first, setFirst ] = useState('')
   const [ last, setLast ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
-  const [ user, setUser ] = useState([])
 
   // Component Functions
   // Create user on backend
-  const handleSubmit = async (e) => {
+  const handleSignup = (e) => {
     try {
-      e.preventDefault();
+      e.preventDefault()
       // Temporarily set 'location' as false
       const location = false;
-      const response = await axios.post(`${env.REACT_APP_BACKEND_URL}/user`, {first, last, email, password, location})
-      // Console log response from backend
-      console.log({response})
-      // Set user state
-      setUser(response.data.user)
-      // Set localstorage with user id
-      localStorage.setItem('userId', response.data.user.id)
+      axios.post(`${env.REACT_APP_BACKEND_URL}/user`, {first, last, email, password, location})
+      .then((response)=>{setUser(response.data.user); localStorage.setItem('dashboard-token', response.data.user.id)})
     } catch (error) {
       console.log(error.message)
     }
@@ -32,9 +33,9 @@ const SignUp = () => {
 
   return (
     <div className='signUp'>
-      <h1>SignUp</h1>
+      <h1>Sign Up</h1>
       <div className='signUpForm'>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSignup}>
           <label htmlFor='first'>First Name:</label>
           <input type='text' name='first' placeholder='First Name *' value={first} onChange={(e)=>{setFirst(e.target.value)}} />
           <label htmlFor='last'>Last Name:</label>
@@ -42,8 +43,8 @@ const SignUp = () => {
           <label htmlFor='email'>Email Address:</label>
           <input type='text' name='email' placeholder='Email Address *' value={email} onChange={(e)=>{setEmail(e.target.value)}} />
           <label htmlFor='password'>Password:</label>
-          <input type='password' name='first' placeholder='Password' value={password} onChange={(e)=>{setPassword(e.target.value)}} />
-          <input type='submit' value='Sign-up'/>
+          <input type='password' name='first' placeholder='Password *' value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+          <input type='submit' value='Sign up'/>
         </form> 
         <div>* required fields</div>
       </div>
@@ -51,4 +52,4 @@ const SignUp = () => {
   )
 }
 
-export default SignUp
+export default Signup
