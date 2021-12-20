2d595d5220df203f69009479060ced5dfdaf74c1
wonaval <wonaval@gmail.com>
2021-12-19 22:31:29 -0500

    Aded footer and greeting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,6 @@
 const Footer = () => {
   return (
     <div>dashboard 2021 - wonaval</div>
-
-    // <div className='footer'>
-    // <div className="text">dashboard</div>
-    // </div>
   )
 }
 export default Footer
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -21,12 +21,12 @@ const Greeting = () => {
     // Generate today's date
     const newDate = new Date()
     // Get numeric month then convert to string
-    const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December" ]
+    const monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September', 'October', 'November', 'December' ]
     const monthString = monthNames[newDate.getMonth()-1]
     // Get numeric date
     const date = newDate.getDate()
     // Get day string
-    const dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+    const dayNames = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
     const day = dayNames[newDate.getDay()]
     // Based upon date get ordinal suffix
     let suffix;
@@ -34,18 +34,18 @@ const Greeting = () => {
       case 1: 
       case 21:
       case 31:
-      suffix = "st"
+      suffix = 'st'
       break
       case 2:
       case 22:
-      suffix = "nd"
+      suffix = 'nd'
       break
       case 3:
       case 23:
-      suffix = "rd"
+      suffix = 'rd'
       break
       default: 
-      suffix = "th"
+      suffix = 'th'
     }
     const outputDate = `${day} ${monthString} ${date}${suffix}, ${newDate.getFullYear()}`
     setDate(outputDate)
